9d1182e8315d44da1877d4ca717a091b11aa3c4b
AhmedHassan <mrahmedhassan95@gmail.com>
2021-08-18 15:21:23 +0200

    finish the setup env

---
+++ src/OOP/PHP/Employee.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\OOP\PHP;
+
+class Employee {
+    private string $name;
+    private int $age;
+
+    public function __construct(string $name, int $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+}
---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php 
+require_once __DIR__.'/../vendor/autoload.php';
+
+use App\OOP\PHP\Employee;
+
+$ahmed = new Employee('ahmed',15);
+var_dump($ahmed);
